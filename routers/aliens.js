5fbb937a77948e272ce755c3b63a73afc1613e5b
jashwant pradhan <jpradhan727@gmail.com>
2020-06-05 18:40:39 +0530

    second commit

--- routers/aliens.js
+++ routers/aliens.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Aliens = require('../models/alien')
 module.exports = router
-
-router.get('/',(req,res)=>{
-    res.send('Get Request')
+router.get('/',async(req,res)=>{
+    try{
+            const aliens = await Aliens.find()
+            res.json(aliens)
+    }catch(err){
+        res.send('Error'+err)
+    }
 })
 
